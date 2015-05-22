cb34e9e11f417a646a1ab670e2b40b366210e876
sciking <sciking@users.noreply.github.com>
2015-05-22 21:13:27 +0200

    Inseriti eventi casuali

--- malloppo.py
+++ malloppo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/ python
 # -*- coding: utf-8 -*-
 import os 
 os.system("clear")
@@ -11,6 +11,7 @@ turno = 1
 nuovo = 0
 #ord3 = 0
 ordtot = 0
+evadi = 0
 soldi = 10000
 print """ Benvenuto. Il tuo obiettivo è quello di truffare più persone possibili. Sfrutta al massimo i primi cinque turni in cui non puoi essere arrestato e in cui tutti presumeranno la tua buona fede."""
 print " Gioco creato da Sciking"
@@ -18,6 +19,96 @@ tuonome = raw_input("Come ti chiami?: ")
 nome = raw_input("Come vuoi chiamare il tuo negozio? ")
 raw_input("Premi invio per iniziare:")
 os.system("clear")
+def poss():
+	global soldi
+	poss = random.randint(1,20)
+	if poss == 1:
+		print "Hai vinto 1000 Euro al lotto"
+		soldi = soldi + 1000
+		gioco()
+	elif poss == 2:
+		print "Hai ricevuto una multa di 1000 Euro dalla Gendarmeria Fiscale"
+		soldi = soldi-1000
+		gioco()
+	elif poss == 3:
+		print "Un fornitore polacco ti trova dei cellulari più economici e guadagni 700 Euro"
+		soldi = soldi + 700
+		gioco()
+	elif poss == 4:
+		print "Dei ladri ti hanno rapinato il negozio. Perdi 5000€, ma l'assicurazione te ne ridà 3000"
+		soldi = soldi - 2000
+		gioco()
+	elif poss == 5:
+		print "Riesci a truffare il fisco per 15000€, ma per evitare l'arresto fuggi in Svizzera."
+		print "Benvenuto in Svizzera, signor", tuonome
+		print "La polizia ha chiuso", nome, "al turno", turno
+		soldi = soldi + 15000
+		print "Hai ottenuto", soldi
+		exit()
+	elif poss == 6:
+		print "Tenti di andare in Slovenia per ripulire soldi"
+		print "Buongiorno signore"
+		print "Lei è in stato di fermo"
+		print "La polizia slovena arresta", tuonome,	"e fa chiudere", nome
+		print "Sono stati sequestrati Euro", soldi
+		exit()
+	elif poss == 7:
+		print "Il tuo fornitore sammarinese ti fa degli sconti per l'abbassamento dell'IVA locale. Prendi il 10% dei tuoi soldi"
+		soldi = soldi*1.10
+		gioco()
+	elif poss == 8:
+		print "Aumenta l'IVA. Perdi il 3 % dei tuoi soldi"
+		gioco()
+	elif poss == 9:
+		print "Vieni recensito da un blog famoso e ottieni molti nuovi ordini!"
+		soldi = soldi*1.5
+		gioco()
+	elif poss == 10:
+		print "Paghi 5000€ un noto blogger per ottenere una buona recensione"
+		soldi = soldi - 50000
+	elif poss == 11:
+		print "Un uomo travestito da panino che fa versi da scimmia vicino a", nome,"fa divertire i turisti"
+		gioco()
+	elif poss == 12:
+		print "Vai in un postribolo d'alto bordo con un tuo fornitore. Spendi 1500€"
+		soldi = soldi - 1500
+		gioco()
+	elif poss == 13:
+		print "Trovi 688€ davanti al negozio"
+		soldi = soldi + 688
+		gioco()
+	elif poss == 14:
+		print "Tuo figlio vince la gara di rutti in dialetto indetta dalla scuola"
+		gioco()
+	elif poss == 15:
+		print "Ottieni un rimborso IVA di 2500€"
+		soldi = soldi + 2500
+		gioco()
+	elif poss == 16:
+		print "Hai immatricolato male il veicolo, vieni multato dalla Polizia"
+		soldi = soldi*0.97
+		gioco()
+	elif poss == 17 and evadi < 40:
+		print "Sei stato denunziato per attività scorrette"
+		print "Sei multato per 100000€"
+		soldi = soldi - 100000
+		gioco()
+	elif poss == 18 and evadi > 50:
+		print "Hai vinto un premio di stato di 25000€"
+		soldi = soldi + 25000
+		gioco()
+	elif poss == 19:
+		print "Vinci 1000 € alla lotteria"
+		soldi = soldi +1000
+		gioco()
+	elif poss == 20:
+		print "Fallisci investimenti. Perdi il 30% dei tuoi soldi"
+		soldi = soldi*0.7
+		gioco()
+	else:
+		gioco()
+	gioco()
+	
 def gioco():
 	global turno
 	#global ord1
@@ -37,7 +128,7 @@ def gioco():
 	try:
 		while evadi <= 0:
 			evadi = input("Quanti ordini vuoi evadere?")
-	except SyntaxError:
+	except SyntaxError or NameError:
 		print "Errore!"
 		os.system("clear")
 		global soldi
@@ -57,13 +148,13 @@ def gioco():
 		if l == 3:
 			print "sono un anonimo benefattore e salverò la tua azienda"
 			soldi = 1000
-			gioco()
+			poss()
 		else:
 			print "Gendarmeria Fiscale. Lei è in arresto per fallimento"
 			print "La Gendarmeria Fiscale ha chiuso", nome
 			print nome, "viene scarcerato subito e non potrà avviare imprese per 5 anni"
 			exit()
-	while ordtot > evadi*6 or evadi < 10 and soldi > 50000:
+	while ordtot > evadi*6 or evadi < 30 and soldi > 50000:
 		k =random.randint(1,10)
 		print k 
 		if k == 8:
@@ -79,10 +170,10 @@ def gioco():
 			print "Paga il 5% per coprire tutto"
 			soldi = soldi*0.95
 			os.system("clear")
-			gioco()
+			poss()
 		else:
 			os.system("clear")
-			gioco()
+			poss()
 			
 	if svizzera == "scappa":
 		suisse = random.randint(1,6)
@@ -91,6 +182,7 @@ def gioco():
 			print "Sei in arresto per truffa e contrabbando"
 			print "La Polizia ha chiuso", nome, "e ha sequestrato", soldi*0.66
 			print "Il giorno dopo i giornali titolano 'Arrestato", tuonome, "al valico di frontiera'"
+			exit()
 		elif suisse == 3:
 			print "C'è stato un grave problema. Perdi metà dei tuoi soldi"
 			print "Sei fuggito in Svizzera con", soldi/2
@@ -98,7 +190,7 @@ def gioco():
 		elif suisse == 6:
 			print "Signor", tuonome, "i suoi documenti non sono validi"
 			print "La Svizzera ti ha respinto, torni in Italia."
-			gioco()
+			poss()
 		else:
 			print "Benvenuto in Svizzera, signor", tuonome
 			print "Sei stato ammesso in Svizzera con Euro", soldi*0.66
@@ -106,7 +198,7 @@ def gioco():
 			exit()
 	else:
 		os.system("clear")
-		gioco()
+		poss()
 	
 
 	
@@ -130,7 +222,7 @@ def gioco5():
 		try:
 			while evadi <= 0:
 				evadi = input("Quanti ordini vuoi evadere?")
-		except SyntaxError:
+		except SyntaxError or NameError:
 			print "errore!"
 			global soldi
 			global nuovi
@@ -146,6 +238,3 @@ def gioco5():
 		os.system("clear")
 	gioco()
 gioco5()		
-
-	
-	
